045bb348c2566ed94c6652c80f47b65b97b46e04
Adria Hortoenda <tuadria@gmail.com>
2025-03-06 10:47:39 +0000

    modificada la linea 3 por adria

--- carpetaProyecto/prueba1.py
+++ carpetaProyecto/prueba1.py
@@ -1,7 +1,9 @@
 # fichero de prueba para el git
 
-print('Hello, World!') #creando conflicto en nueva-rama
+print('Hello, World! v1') #creando conflicto en nueva-rama 
 
 # ahora hago modificaciones que no se tendran que ver en main
 
 print('esto se hace desde "nueva-rama".')
+
+print('nueva linea') # se añadira todos los cambios de nueva-rama a main
